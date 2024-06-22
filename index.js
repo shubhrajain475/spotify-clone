ffcb7f006e41395c85b52656705f0d710cfb87b6
shubhra <shubhrajain380@gmail.com>
2024-06-22 17:10:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,162 @@
+// let arr=[1,2,3,4,5,6,2,3];
+// let num=2;
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]==num){
+//         arr.splice(i,1);
+//     }
+// }
+// console.log(arr);
+
+// let number=287152;
+// let count=0;
+// let copy=number;
+
+// while(copy>0){
+//     count++;
+//     copy=Math.floor(copy/10);
+// }
+// console.log(count);
+
+// let number=287152;
+// let sum=0;
+// let copy=number;
+// while(copy>0){
+//     digit=copy%10;
+//     sum+=digit;
+//     copy=Math.floor(copy/10);
+// }
+// console.log(sum);
+
+
+// let arr=[1,2,4,9,8,7,6,3];
+// let num=5;
+// function getElement(arr,num){
+//     for(let i=0;i<arr.length;i++){
+//         if(arr[i]>num){
+//             console.log(arr[i]);
+//         }    }
+// }
+// getElement(arr,num);
+
+
+
+
+// let str="abcadfdfdfdfdfdfdf";
+// function getUnique(str){
+//  let ans="";
+//  for(let i=0;i<str.length;i++){
+// let currChar=str[i];
+// if(ans.indexOf(currChar)==-1){
+//     ans+=currChar
+// }
+//  }
+//  return ans;
+// }
+// console.log(getUnique(str));
+
+
+
+// let country=["india","america"];
+// function longestName(country){
+//     let ansIdx=0;
+//     for(let i=0;i<country.length;i++){
+//         let ansLen=country[ansIdx].length;
+//         let currLen=country[i].length;
+//         if(currLen>ansLen){
+//             ansIdx=i;
+//         }
+//     }
+//     return country[ansIdx];
+// }
+// console.log(longestName(country));
+
+// const arrayAverage=(arr)=>{
+//     let total=0;
+//     for(let numbers of arr){
+//         total+=numbers;
+//     }
+//     return total/arr.length;
+// }
+
+
+// let arr=[1,2,3,4,5,6];
+// console.log(arrayAverage(arr));
+
+
+let num=4;
+const isEven=(num)=>{
+num%2==0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
